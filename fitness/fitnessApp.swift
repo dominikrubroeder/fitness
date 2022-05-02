0d1a3077ce29db20bb390fe7aac628ce62eb7119
Dominik <dominik.rubroeder@icloud.com>
2022-05-02 15:15:35 +0200

    Initial Commit

---
+++ fitness/fitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitnessApp.swift
+//  fitness
+//
+//  Created by Dominik Rubröder on 02.05.22.
+//
+
+import SwiftUI
+
+@main
+struct fitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
